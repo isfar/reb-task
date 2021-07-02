d25941caefc3bd36d9ae07e1c218352c1c43ef0c
Isfar Sifat <isfar.sifat@gmail.com>
2021-07-02 22:22:29 +0600

    Added the FizBuzz service

---
+++ src/FizBuzz.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Isfar;
+
+use InvalidArgumentException;
+
+class FizBuzz
+{
+    public function fizzBuzz($start = 1, $stop = 100)
+    {
+        $string = '';
+
+        if ($stop < $start || $start < 0 || $stop < 0) {
+            throw new InvalidArgumentException();
+        }
+
+        for ($i = $start; $i <= $stop; $i++) {
+            if ($i % 3 == 0 && $i % 5 == 0) {
+                $string .= 'FizzBuzz';
+                continue;
+            }
+
+            if ($i % 3 == 0) {
+                $string .= 'Fizz';
+                continue;
+            }
+
+            if ($i % 5 == 0) {
+                $string .= 'Buzz';
+                continue;
+            }
+
+            $string .= $i;
+        }
+
+        return $string;
+    }
+}
